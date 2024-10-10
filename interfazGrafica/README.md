a8e78ece9c701c764f3e6215329865f330077c5b
LRosales420 <luisjosuerosales@gmail.com>
2024-10-09 20:45:47 -0600

    commit

---
+++ interfazGrafica/README.md
@@ -0,0 +1,17 @@
+#Calculadora con interfaz gráfica
+
+En el presente directorio, se utilizó el módulo Tkinter para crear una interfaz gráfica para una calculadora.
+
+(Explicación)
+
+#Instrucciones de uso
+
+¿Qué hay que tener instalado para ejecutar el programa? ¿Qué comando utilizar?
+
+```
+py calculadora.py
+```
+
+#Demostración de ejecución
+
+(Adjuntar imagen de la calculadora ejecutándose)
--- interfazGrafica/interfazGrafica.py
+++ interfazGrafica/interfazGrafica.py
@@ -1,9 +1,23 @@
 import tkinter as tk
 
 root = tk.Tk()
+
+"""
 root.title("Mi primera ventana :D")
 root.geometry("300x200")
+"""
 
-root.mainloop
+root.geometry("500x200")
 
-tk._test()
+label = tk.Label(root, text="Cuando yo la ví Jujuju", font=("Comic Sans MS", 24), fg="green")
+label.pack()
+
+entry = tk.Entry(root)
+entry.pack()
+
+button = tk.Button(root, text="Borrar la cuenta", font=("Papyrus", 16), bg="red", fg="blue", anchor="se")
+button.pack()
+
+
+
+root.mainloop()
